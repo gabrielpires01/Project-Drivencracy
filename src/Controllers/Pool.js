dbddd4403a3bba375659d6cbff88276dffd6fea6
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-02-17 01:06:48 -0300

    Post pool Route complete

---
+++ src/Controllers/Pool.js
@@ -0,0 +1,24 @@
+import { db } from "../Database/Database.js";
+
+const PostPool = async (req,res) => {
+	let {title, expireAt} = req.body;
+	if (!title) return res.sendStatus(422)
+	if(!expireAt)  {
+		const today	= new Date();
+		let newExpire = new Date().setDate(today.getDate() + 30)
+		newExpire = new Date(newExpire);
+		const date = newExpire.toISOString().split('T')[0];
+		const time = newExpire.toTimeString().split(' ')[0];
+		expireAt = `${date} ${time}`
+	}
+	
+	try {
+		await db.collection("pools").insertOne({title,expireAt})
+		return res.sendStatus(201)
+	} catch(err) {
+		return res.sendStatus(err)
+	}
+
+};
+
+export {PostPool}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { PostPool } from "./Controllers/Pool.js";
 
 const app = express();
 app.use(express.json());
+
+app.post('/pool', PostPool)
 
 export default app;
