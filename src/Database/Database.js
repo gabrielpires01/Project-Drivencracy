2a3103776558fa411b46d5f92cebb59891036431
Gabriel Pires <gabriel.pires@twistsystems.com>
2022-02-17 04:37:35 -0300

    Adjust code for deploy heroku

--- src/Database/Database.js
+++ src/Database/Database.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
